19d9aa26de36e0e60f9eb5f9d0f986b47ea6794b
Manish Rawat <manishrawat4u@gmail.com>
2021-04-17 11:32:49 -0400

    concurrency support added

--- src/BatchUploadRequestProcessor.ts
+++ src/BatchUploadRequestProcessor.ts
@@ -4,7 +4,11 @@ import { handleUploadProgress, handleError } from "./RemoteUploadApiMethods";
 import { uploadAsync } from "./UploadUtils";
 import delay from 'delay';
 import debug from 'debug';
+import PQueue from 'p-queue';
 const processingDelay = parseInt(process.env.PROCESS_BATCH_DELAY || '60000');   //in milli seconds
+const maxQueueConcurrency = parseInt(process.env.CONCURRENCY_MAX || '2');   //in milli seconds
+
+const queue = new PQueue({ concurrency: maxQueueConcurrency });
 
 const logger = debug('BatchUploadRequestProcessor');
 export class BatchUploadRequestProcessor {
@@ -27,16 +31,23 @@ const processQueuedItems = async () => {
 
     logger(`queued items found ${queuedItems.length}`);
     for (const queuedItem of queuedItems) {
-        try {
-            const progressReporter = (prog: UploadProgress) => {
-                handleUploadProgress(queuedItem, prog);
-            };
-            await AuthenticatedApiClient.post(`remoteUrlUploadRequest/${queuedItem.id}/start`);
-            await uploadAsync(queuedItem, progressReporter);
-            await AuthenticatedApiClient.post(`remoteUrlUploadRequest/${queuedItem.id}/complete`);
-        } catch (error) {
-            logger(error);
-            handleError(queuedItem, error);
-        }
+        logger('checking if queue can handle this...');
+        await queue.onSizeLessThan(maxQueueConcurrency);
+        logger('checking is ready to handle this... queueing up!!!');
+        queue.add(() => processItem(queuedItem));
+    }
+}
+
+async function processItem(queuedItem: RequestItemResponse) {
+    try {
+        const progressReporter = (prog: UploadProgress) => {
+            handleUploadProgress(queuedItem, prog);
+        };
+        await AuthenticatedApiClient.post(`remoteUrlUploadRequest/${queuedItem.id}/start`);
+        await uploadAsync(queuedItem, progressReporter);
+        await AuthenticatedApiClient.post(`remoteUrlUploadRequest/${queuedItem.id}/complete`);
+    } catch (error) {
+        logger(error);
+        handleError(queuedItem, error);
     }
 }

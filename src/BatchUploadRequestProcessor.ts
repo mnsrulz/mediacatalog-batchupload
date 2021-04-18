66d3389eee2a212f1e34022c69d04b7beb2783eb
mnsrulz <>
2021-04-17 20:04:03 -0400

    signal feature added

--- src/BatchUploadRequestProcessor.ts
+++ src/BatchUploadRequestProcessor.ts
@@ -5,20 +5,25 @@ import { uploadAsync } from "./UploadUtils";
 import delay from 'delay';
 import debug from 'debug';
 import PQueue from 'p-queue';
-const processingDelay = parseInt(process.env.PROCESS_BATCH_DELAY || '60000');   //in milli seconds
+import AbortController from "abort-controller";
+
+const processingDelay = parseInt(process.env.PROCESS_BATCH_DELAY || '300000');   //in milli seconds
 const maxQueueConcurrency = parseInt(process.env.CONCURRENCY_MAX || '2');   //in milli seconds
 
 const queue = new PQueue({ concurrency: maxQueueConcurrency });
 
 const logger = debug('BatchUploadRequestProcessor');
-
+let abortController: AbortController = new AbortController();
 export const startProcessing = async () => {
     while (true) {
-        await processQueuedItems();
         logger('processing...');
-        await delay(processingDelay);
+        await processQueuedItems();
+        await delay(processingDelay, { signal: abortController.signal }).catch(() => { logger('signal must have aborted this') });
+        abortController = new AbortController();
     }
 }
+
+export const signal = () => abortController.abort();
 
 const processQueuedItems = async () => {
     const queuedItems = await AuthenticatedApiClient<RequestItemResponse[]>('remoteUrlUploadRequest', {
@@ -29,7 +34,7 @@ const processQueuedItems = async () => {
     });
 
     logger(`queued items found ${queuedItems.length}`);
-    for (const queuedItem of queuedItems) {
+    for (const queuedItem of queuedItems.reverse()) {   //reverse the list as old items should process first
         logger('checking if queue can handle this...');
         await queue.onEmpty();
         logger('checking is ready to handle this... queueing up!!!');
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {startProcessing} from "./BatchUploadRequestProcessor";
+import {startProcessing, signal} from "./BatchUploadRequestProcessor";
 
 import express from 'express';
 const app = express();
@@ -14,6 +14,11 @@ const port = process.env.PORT || 3000;
 
 app.get('/process', (req, res) => {
   res.send('we are on it...');
+})
+
+app.get('/trigger', (req, res) => {
+  signal();
+  res.send('Ack!!!');
 })
 
 app.get('*', (req, res) => {
